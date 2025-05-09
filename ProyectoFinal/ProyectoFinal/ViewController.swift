18279114cee523479b32d388fdfa18323b6f7012
alumno <alumno@uacj-9aa4b7e1dc.uacj.mx>
2025-05-09 09:57:05 -0600

    primer commit

---
+++ ProyectoFinal/ProyectoFinal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProyectoFinal
+//
+//  Created by alumno on 5/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
